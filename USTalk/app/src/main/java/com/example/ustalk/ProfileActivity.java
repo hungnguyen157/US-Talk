8acef7ec1180287ecfeb129dc16e52a8933f626f
NguyenQuocThong2603 <63840452+NguyenQuocThong2603@users.noreply.github.com>
2022-03-30 11:26:55 +0700

    Profile and Edit Profile

--- USTalk/app/src/main/java/com/example/ustalk/ProfileActivity.java
+++ USTalk/app/src/main/java/com/example/ustalk/ProfileActivity.java
@@ -2,13 +2,34 @@ package com.example.ustalk;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
 
-public class ProfileActivity extends AppCompatActivity {
-
+public class ProfileActivity extends Activity implements View.OnClickListener {
+    ImageButton btnBack;
+    Button btnEdit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        btnBack = (ImageButton) findViewById(R.id.btnBack);
+        btnEdit = (Button) findViewById(R.id.btnEdit);
+        btnEdit.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnEdit:{
+                startActivity(new Intent(getApplicationContext(), EditProfile.class));
+                break;
+            }
+        }
     }
 }
