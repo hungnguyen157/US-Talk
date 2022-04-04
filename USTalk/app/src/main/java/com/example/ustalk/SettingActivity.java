f13ab5ce126b663b9efd6ec2dd46cb9a1c6dd344
NguyenQuocThong2603 <63840452+NguyenQuocThong2603@users.noreply.github.com>
2022-04-04 14:49:19 +0700

    Show information in setting activity

--- USTalk/app/src/main/java/com/example/ustalk/SettingActivity.java
+++ USTalk/app/src/main/java/com/example/ustalk/SettingActivity.java
@@ -4,22 +4,54 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.example.ustalk.utilities.CurrentUserDetails;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingActivity extends Activity implements View.OnClickListener {
     LinearLayout Profile;
     LinearLayout Password;
     LinearLayout Notifiation;
+    ImageView btnBack;
+    FirebaseFirestore db;
+    TextView txtName;
+    CircleImageView profileImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        db = FirebaseFirestore.getInstance();
         Profile = (LinearLayout) findViewById(R.id.profile_navigate);
         Password = (LinearLayout) findViewById(R.id.password_navigate);
         Notifiation = (LinearLayout) findViewById(R.id.notification_navigate);
+        btnBack =(ImageView) findViewById(R.id.btn_back);
+        txtName = (TextView) findViewById(R.id.name);
+        profileImage = (CircleImageView) findViewById(R.id.profile_image);
         Profile.setOnClickListener(this);
         Password.setOnClickListener(this);
         Notifiation.setOnClickListener(this);
+        btnBack.setOnClickListener(this);
+        showInfo();
+    }
+    private void showInfo(){
+        db.collection("users").document(CurrentUserDetails.getInstance().getUid()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        String name = documentSnapshot.getString("name");
+                        String imageProfile = documentSnapshot.getString("imageProfile");
+                        txtName.setText(name);
+                        Glide.with(SettingActivity.this).load(imageProfile).into(profileImage);
+                    }
+                });
     }
 
     @Override
@@ -36,5 +68,7 @@ public class SettingActivity extends Activity implements View.OnClickListener {
         {
 
         }
+        else if (view.getId()==btnBack.getId())
+            onBackPressed();
     }
 }
