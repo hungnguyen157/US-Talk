2f6e01ee201e3b12d4f0a4d256c489e6506fee47
NguyenQuocThong2603 <63840452+NguyenQuocThong2603@users.noreply.github.com>
2022-04-04 13:46:13 +0700

    complete update profile

--- USTalk/app/src/main/java/com/example/ustalk/ProfileActivity.java
+++ USTalk/app/src/main/java/com/example/ustalk/ProfileActivity.java
@@ -1,9 +1,14 @@
 package com.example.ustalk;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.accessibilityservice.AccessibilityService;
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.ContentProvider;
+import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.res.ColorStateList;
@@ -12,8 +17,10 @@ import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
@@ -21,11 +28,30 @@ import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.example.ustalk.databinding.ActivityProfileBinding;
+import com.example.ustalk.models.User;
+import com.example.ustalk.utilities.CurrentUserDetails;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.util.HashMap;
 import java.util.Objects;
 
 public class ProfileActivity extends Activity implements View.OnClickListener {
@@ -57,18 +83,19 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
     RadioGroup sex_field;
     RadioButton rbtn_male, rbtn_female;
     Button btnEdit, btnSave, btnCancel;
-    private BottomSheetDialog bottomsheet;
+    TextView editEmail;
     private int IMAGE_GALLERY_REQUEST = 3;
-    //data variables
-    String current_name = "Nguyễn Quốc Thông";
-    int current_sex = R.id.rbtn_male;
-
+    private Uri imageUri;
+    FirebaseFirestore fireStore;
+    ProgressDialog progressDialog;
+    int flag =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
-
+        fireStore = FirebaseFirestore.getInstance();
         //get widgets
+        editEmail = (TextView) findViewById(R.id.editEmail);
         btnBack = (ImageView) findViewById(R.id.btnBack);
         profile_image = (ImageView) findViewById(R.id.profile_image);
         fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -79,14 +106,18 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
         btnEdit = (Button) findViewById(R.id.btnEdit);
         btnSave = (Button) findViewById(R.id.btnSave);
         btnCancel = (Button) findViewById(R.id.btnCancel);
-
-        //set data for sex_field
-        if (current_sex == rbtn_male.getId()){
-            rbtn_male.setChecked(true);
-        }
-        else{
-            rbtn_female.setChecked(true);
-        }
+//        User user =CurrentUserDetails.getInstance().getUser();
+//        CurrentUserDetails.getInstance().updateUser(user);
+//        String sex = user.sex;
+//        editEmail.setText(user.email);
+//        editName.setText(user.name);
+//        if(sex.equals("Nam"))
+//            rbtn_male.setChecked(true);
+//        else
+//            rbtn_female.setChecked(true);
+//        Glide.with(ProfileActivity.this).load(user.imageProfile).into(profile_image);
+        showInfo();
+        progressDialog = new ProgressDialog(this);
 
         //disable EditText and RadioGroup
         editName.setEnabled(false);
@@ -120,7 +151,7 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
             }
             case (R.id.btnEdit):{
                 editName.setEnabled(true);
-                editName.requestFocus(current_name.length()-1);
+                editName.requestFocus(editName.getText().toString().length()-1);
                 rbtn_male.setEnabled(true);
                 rbtn_male.setButtonTintList(ColorStateList.valueOf(getColor(R.color.orange)));
                 rbtn_female.setEnabled(true);
@@ -132,11 +163,7 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
                 break;
             }
             case (R.id.btnSave):{
-                String new_name = editName.getText().toString();
-                int new_sex = sex_field.getCheckedRadioButtonId();
-                if (!new_name.equals(current_name) || new_sex != current_sex){
-                    //do the save work
-                }
+                uploadToFirebase();
             }
             case (R.id.btnCancel):{
                 editName.setEnabled(false);
@@ -148,10 +175,42 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
                 btnSave.setVisibility(View.GONE);
                 btnCancel.setVisibility(View.GONE);
                 fab.setVisibility(View.GONE);
+                showInfo();
                 break;
             }
         }
     }
+    private void showInfo(){
+//        User user =CurrentUserDetails.getInstance().getUser();
+//        CurrentUserDetails.getInstance().updateUser(user);
+//        String sex = user.sex;
+//        editEmail.setText(user.email);
+//        editName.setText(user.name);
+//        if(sex.equals("Nam"))
+//            rbtn_male.setChecked(true);
+//        else
+//            rbtn_female.setChecked(true);
+//        Glide.with(ProfileActivity.this).load(user.imageProfile).into(profile_image);
+        fireStore.collection("users").document(CurrentUserDetails.getInstance().getUid()).get()
+                .addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                    @Override
+                    public void onSuccess(DocumentSnapshot documentSnapshot) {
+                        if(documentSnapshot.exists()){
+                            String email = documentSnapshot.getString("email");
+                            String userName = documentSnapshot.getString("name");
+                            String sex = documentSnapshot.getString("sex");
+                            String imageProfile = documentSnapshot.getString("imageProfile");
+                            editName.setText(userName);
+                            editEmail.setText(email);
+                            if(sex.equals("Nam"))
+                                rbtn_male.setChecked(true);
+                            else
+                                rbtn_female.setChecked(true);
+                            Glide.with(ProfileActivity.this).load(imageProfile).into(profile_image);
+                    }
+                }
+    });
+}
 
     private void openGallery(){
         Intent intent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
@@ -161,8 +220,59 @@ public class ProfileActivity extends Activity implements View.OnClickListener {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if(resultCode==RESULT_OK&&data!=null){
-            Uri imageUri = data.getData();
+            imageUri = data.getData();
             profile_image.setImageURI(imageUri);
+            flag =1;
+        }
+    }
+    private void uploadToFirebase(){
+        int selectedID = sex_field.getCheckedRadioButtonId();
+        RadioButton selectRadio = (RadioButton) findViewById(selectedID);
+//        DocumentReference mountainRef = fireStore.collection("users").document(CurrentUserDetails.getInstance().getUid());
+//        mountainRef.update("name",editName.getText().toString(),"sex",selectRadio.getText().toString())
+//            .addOnSuccessListener(new OnSuccessListener<Void>() {
+//                @Override
+//                public void onSuccess(Void unused) {
+//                    flag1++;
+//                }
+//            });
+        HashMap<String,Object> hashmapName =new HashMap<>();
+        hashmapName.put("name",editName.getText().toString());
+//        fireStore.collection("users").document(CurrentUserDetails.getInstance().getUid()).update(hashmapName);
+//        HashMap<String, Object> hashmapSex =new HashMap<>();
+        hashmapName.put("sex",selectRadio.getText().toString());
+        fireStore.collection("users").document(CurrentUserDetails.getInstance().getUid()).update(hashmapName);
+        if(imageUri!=null&&flag==1){
+            progressDialog.setMessage("Đang cập nhật...");
+            progressDialog.show();
+            StorageReference storageRef = FirebaseStorage.getInstance().getReference();
+            StorageReference mountainsRef = storageRef.child("Avatar/"+System.currentTimeMillis()+CurrentUserDetails.getInstance().getUid());
+            mountainsRef.putFile(imageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Task <Uri> urlTask = taskSnapshot.getStorage().getDownloadUrl();
+                    while(!urlTask.isSuccessful());
+                    Uri downloadUrl = urlTask.getResult();
+                    String url = downloadUrl.toString();
+                    HashMap<String, Object> hashmap =new HashMap<>();
+                    hashmap.put("imageProfile",url);
+                    fireStore.collection("users").document(CurrentUserDetails.getInstance().getUid()).update(hashmap)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            showInfo();
+                            Toast.makeText(getApplicationContext(), "Cập nhật thành công", Toast.LENGTH_SHORT).show();
+                            progressDialog.dismiss();
+                            flag = 0;
+                        }
+                    });
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressDialog.dismiss();
+                }
+            });
         }
     }
 }
